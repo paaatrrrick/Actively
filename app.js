2e464531840b7fec441629eccb644c7aaf04ee44
paaatrrrick <patrick.123.foster@gmail.com>
2022-05-27 21:53:46 -0500

    fksjfls

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(passport.initialize());
 // Step 1:
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
-app.get("*", function (request, response) {
+app.get("/", function (request, response) {
     response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
